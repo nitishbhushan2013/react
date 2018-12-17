002a5acb644f89f5269922c4338867d6480e7758
Nitish Bhushan <nitishbhushan2013@gmail.com>
2018-12-17 23:03:56 +1100

    reactJS - first ever program : hello world

---
+++ helloworld/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const helloworld = < h1 > Hello World!!Welcome to reactJS < /h1>
+
+ReactDOM.render(helloworld, document.getElementById('root'));
