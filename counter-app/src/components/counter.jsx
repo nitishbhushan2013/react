8718c408f4f821d1f5a89323b4025a0954ab3dc5
Nitish Bhushan <nitishbhushan2013@gmail.com>
2018-12-17 23:33:02 +1100

    react application - counter-app

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    // view component
+    return <h1> first react component</h1>;
+  }
+}
+export default Counter;
