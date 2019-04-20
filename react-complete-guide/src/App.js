6785a87a9b0cef53928392b898d2170bbe3e9aba
Nitish Bhushan <nitishbhushan2013@gmail.com>
2019-04-20 12:50:55 +1000

    state attribute of class based Component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = { // state is an object 
+    persons : [
+      {name : "Nitish", company : "Google"},
+      {name : "Matt", company : "Facebook"},
+      {name : "Arthur", company : "DropBox"}
+    ]
+  }
+// when ever state value changes, react would re-render the component or rather update the view.
   render() {
     return (
       <div className="App">
-         <Person name = "Nitish" company = "Google"/>
-         <Person name = "Matt" company = "Facebook"/>
-         <Person name = "Arthur" company = "DropBox"> Arthur stay at Sydney. </Person>
+         <Person name = {this.state.persons[0].name} company = {this.state.persons[0].company}/>
+         <Person name = {this.state.persons[1].name} company = {this.state.persons[1].company}/>
+         <Person name = {this.state.persons[2].name} company = {this.state.persons[2].company}> Arthur stay at Sydney. </Person>
       </div>
     );
   }
