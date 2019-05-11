9ccd30bad1c07faf879725f8bb331a7251ed2915
Nitish Bhushan <nitishbhushan2013@gmail.com>
2019-05-12 06:23:44 +1000

    burger builder - burger ingredient view

--- react-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     //life cycle method
    render(){
        return(
            <Aux>
-               <div className="container">
-                    <div>Burger </div>
+               <div className="Container">
+                    <Burger/>
                     <div>Burger Control</div>
                </div>
            </Aux>
