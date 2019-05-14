db95426f78a4823285390160385252f5eace5f30
Nitish Bhushan <nitishbhushan2013@gmail.com>
2019-05-14 22:12:36 +1000

    burgerBuilder : dynamic ingredient calculation

--- react-burger-builder/src/components/Burger/Burger.js
+++ react-burger-builder/src/components/Burger/Burger.js
@@ -2,12 +2,20 @@ import React from 'react';
 import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = ()=>{
+const burger = (props)=>{
+    
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igkey => {
+        return [...Array(props.ingredients[igkey])].map((_,i)=>{
+            return <BurgerIngredient key = {igkey+i} type={igkey} />
+        })
+    });
+
+    
     return(
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- react-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,12 +3,21 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients :{
+            salad:1,
+            meat:1,
+            bacon:1,
+            cheese:0
+        }
+    }
     //life cycle method
    render(){
        return(
            <Aux>
                <div className="Container">
-                    <Burger/>
+                    <Burger ingredients={this.state.ingredients}/>
                     <div>Burger Control</div>
                </div>
            </Aux>
