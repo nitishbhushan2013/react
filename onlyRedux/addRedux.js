06d37f439dfe821b2c2d944475ecadb7d0b694ca
Nitish Bhushan <nitishbhushan2013@gmail.com>
2019-09-24 17:21:28 +1000

    subscribe and payload

--- onlyRedux/addRedux.js
+++ onlyRedux/addRedux.js
@@ -14,13 +14,13 @@ const myReducer = ((state=initialState, action)=>{ // ES6 : provide default valu
     //define action behavior
     switch(action.type) {
         case 'add':
-             newState.count += 2;
+             newState.count += action.val;
              break;
         case 'substract':
-            newState.count -=2;
+            newState.count -=action.val;
             break;
         case 'multiply':
-            newState.count *=2;   
+            newState.count *=action.val;   
             break;      
     }
     return newState; // always returns updated state
@@ -28,13 +28,18 @@ const myReducer = ((state=initialState, action)=>{ // ES6 : provide default valu
 
 // define store : it takes reducer as a parameter
 const myStore = createStore(myReducer);
-console.log("Initial state: "+JSON.stringify(myStore.getState()));
-//perform action
-myStore.dispatch({type: 'add'}) // 'add' is action pass to reducer
-console.log("add action : "+JSON.stringify(myStore.getState()));
 
-myStore.dispatch({type: 'substract'})
-console.log("substract action : "+JSON.stringify(myStore.getState()))
+// subscribe will accept function and it will be called when ever there is state change
+myStore.subscribe(()=>{
+    console.log("state changed "+JSON.stringify(myStore.getState()));
+});
+//
+//perform action, passing the payload 
+myStore.dispatch({type: 'add',val:10}) // 'add' is action pass to reducer
+//console.log("add action : "+JSON.stringify(myStore.getState()));
 
-myStore.dispatch({type: 'multiply'})
-console.log("multiply action : "+JSON.stringify(myStore.getState()))
+myStore.dispatch({type: 'substract', val:10})
+///console.log("substract action : "+JSON.stringify(myStore.getState()))
+
+myStore.dispatch({type: 'multiply', val:10})
+//console.log("multiply action : "+JSON.stringify(myStore.getState()))
